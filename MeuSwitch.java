ba778772be39a50235d3608fee24f83cbbc5e89d
Bruno Silva <bidu.henrique@hotmail.com>
2022-04-06 17:39:25 -0300

    0604

---
+++ MeuSwitch.java
@@ -0,0 +1,56 @@
+package Java;
+import java.util.Scanner;
+
+public class MeuSwitch {
+    
+    public static void main(String[] args) {
+        
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o número:");
+        int numero = ler.nextInt();
+
+        // if (numero == 1)
+        // {
+        //     System.out.println("Você digitou um");
+        // }
+        // else if (numero == 2)
+        // {
+        //     System.out.println("Você digitou dois");
+        // }
+        // else if (numero == 3)
+        // {
+        //     System.out.println("Você digitou o três");
+        // }
+        // else if (numero == 4)
+        // {
+        //     System.out.println("Você digitou o quatro");
+        // }
+        // else 
+        // {
+        //     System.out.println("Não conheço esse número");
+        // }
+
+        switch (numero) 
+        {
+            case 1:
+                System.out.println("Você digitou o um");
+            break;
+            case 2:
+                System.out.println("Você digitou o dois");
+            break;
+            case 3:
+                System.out.println("Você digitou o três");
+            break;
+            case 4:
+                System.out.println("Você digitou o quatro");
+            break;
+            default:
+                System.out.println("Não conheço esse número");
+            break;
+        }
+
+        System.out.println("Fim do código!");
+
+    }
+}
